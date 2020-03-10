972d836eb4481c8da569adc61e7c6d0cfcc9fd77
Kris Foster <kris.foster@gmail.com>
2020-03-09 22:59:25 -0700

    Initial commit of an empty lcd library

---
+++ lcd.h
@@ -0,0 +1,7 @@
+; KrisOS - lcd.h
+; Copyright 2020 Kris Foster
+
+.ifndef _LCD_H_
+_LCD_H_ = 1
+
+.endif
---
+++ lcd.s
@@ -0,0 +1,7 @@
+; KrisOS LCD Library
+; Copyright 2020 Kris Foster
+
+.ifndef _LIB_LCD_
+_LIB_LCD = 1
+
+.endif
