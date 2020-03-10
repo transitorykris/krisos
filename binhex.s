8e2a5c567a47a19471cc50fc8b7a86192f6fde20
Kris Foster <kris.foster@gmail.com>
2020-03-09 20:00:24 -0700

    Reflect krisos style

--- binhex.s
+++ binhex.s
@@ -20,31 +20,31 @@ _LIB_BINHEX_ = 1
     .segment "LIB"
 
 binhex:  
-    PHA                   ;save byte
-    AND #%00001111        ;extract LSN
-    TAX                   ;save it
-    PLA                   ;recover byte
-    LSR                   ;extract...
-    LSR                   ;MSN
+    PHA                         ;save byte
+    AND #%00001111              ;extract LSN
+    TAX                         ;save it
+    PLA                         ;recover byte
+    LSR                         ;extract...
+    LSR                         ;MSN
     LSR
     LSR
-    PHA                   ;save MSN
-    TXA                   ;LSN
-    JSR convert           ;generate ASCII LSN
-    TAX                   ;save
-    PLA                   ;get MSN & fall thru
+    PHA                         ;save MSN
+    TXA                         ;LSN
+    JSR convert                 ;generate ASCII LSN
+    TAX                         ;save
+    PLA                         ;get MSN & fall thru
 ;
 ;
 ;   convert nybble to hex ASCII equivalent...
 ;
 convert:
     CMP #$0a
-    BCC final          ;in decimal range
+    BCC final                   ;in decimal range
     ;
-    ADC #$66           ;hex compensate
+    ADC #$66                    ;hex compensate
 ;         
 final:
-    EOR #%00110000     ;finalize nybble
-    RTS                ;done
+    EOR #%00110000              ;finalize nybble
+    RTS                         ;done
 ;
 .endif
