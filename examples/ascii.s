56e8269470a4a09e181a554278c7f887f4b3d46c
Kris Foster <kris.foster@gmail.com>
2020-03-12 21:13:58 -0700

    Add a second example that dumps 256 ascii characers

---
+++ examples/ascii.s
@@ -0,0 +1,32 @@
+; KrisOS - hello.s
+; Copyright 2020 Kris Foster
+;
+; Dump all the ASCII characters
+
+LF      = $0a
+NULL    = $00
+CR      = $0d
+
+; Zero Page pointers
+string_ptr = $00
+
+    .include "stdlib.h"
+
+    .code
+
+main:
+    LDA #<char
+    STA string_ptr
+    LDA #>char
+    STA string_ptr+1
+loop:
+    LDA char
+    CMP #$FF
+    BEQ done
+    JSR write
+    INC char
+    JMP loop
+done:
+    RTS                         ; Return control to KrisOS
+
+char:  .byte   $00, NULL
