3dcb583416d04fca36ea90ef40b3c43ed3a3b602
Kris Foster <kris.foster@gmail.com>
2020-03-15 23:22:37 -0700

    fix up and use the fancy new ACIA constants

--- acia.s
+++ acia.s
@@ -16,10 +16,13 @@ _LIB_ACIA_ = 1
 ; Set up 6551 ACIA
 acia_init:
     PHA
-    STZ ACIA_STATUS             ; Soft reset
+    LDA #ACIA_HARDWARE_RESET
+    STA ACIA_STATUS
     LDA ACIA_DATA               ; Clear errors
-    LDA #%00001011              ; No parity, no echo, no interrupt
+    LDA #(ACIA_TX_DIS_RTS_LOW|ACIA_NO_PARITY|ACIA_NO_ECHO|ACIA_IRQ_DISABLE|ACIA_DTR_ENABLE)
+    ;LDA #%00001011              ; No parity, no echo, no interrupt
     STA ACIA_COMMAND
+    LDA #(ACIA_BAUD_GENERATOR|ACIA_1_STOP_BIT|ACIA_8_DATA_BITS|ACIA_19200_BAUD)
     LDA #%00011111              ; 1 stop bit, 8 data bits, 19200 baud
     STA ACIA_CONTROL
     PLA
