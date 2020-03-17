1290c58b896e09cab3ecc5e04234265db1ce4d58
Kris Foster <kris.foster@gmail.com>
2020-03-16 18:49:39 -0700

    Add an example of how to create an IRQ handler

--- stdlib.h
+++ stdlib.h
@@ -1,3 +1,8 @@
 ; KrisOS - stdlib.h
 
+; Zero page
+nmi_ptr = $FC                   ; Location of NMI routine
+irq_ptr = $FE                   ; Location of IRQ routine
+
+; Library routines
 write = $d000
