380d2d43818840bb1ea6343bab83d79638ac8a3a
Kris Foster <kris.foster@gmail.com>
2020-03-22 20:48:03 -0700

    Final version of the via test

--- examples/via_test.s
+++ examples/via_test.s
@@ -3,9 +3,10 @@
 ;
 ; Fiddles with the VIA ports on the K64
 
-VIA2_PORTA = 5000
-VIA2_DDRA = 5001
-
+VIA2_PORTB = $5000
+VIA2_PORTA = $5001
+VIA2_DDRB = $5002
+VIA2_DDRA = $5003
 
 LF      = $0a
 NULL    = $00
@@ -25,10 +26,22 @@ hello:
     STA string_ptr+1
     JSR write
 
-    LDA #$FF
+ports_on:
+    LDA #$FF                    ; Turn all pin output, and on
     STA VIA2_DDRA
     STA VIA2_PORTA
+    STA VIA2_DDRB
+    STA VIA2_PORTB
+
+done:
+    LDA #<done_msg
+    STA string_ptr
+    LDA #>done_msg
+    STA string_ptr+1
+    JSR write
 
     RTS                         ; Return control to KrisOS
 
 test_msg:  .byte   "Turning LED on", CR, LF, NULL
+done_msg:  .byte   "Done!", CR, LF, NULL
+not_set_msg: .byte "VIA2 did not appear to set correctly",CR,LF,NULL
