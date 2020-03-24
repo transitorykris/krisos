55184a6a72df0fc4a2b9d6161b19fb6c2c07174e
Kris Foster <kris.foster@gmail.com>
2020-03-23 20:48:33 -0700

    This was working once upon a time, 8 bit addresses please

--- kernel_zp.s
+++ kernel_zp.s
@@ -7,7 +7,7 @@ _KERNEL_ZP_ = 1
     .exportzp nmi_ptr
     .exportzp irq_ptr
 
-    .segment "USERVECTORS"
+    .segment "USERVECTORS": zeropage
 nmi_ptr:    .res 2              ; Location of NMI routine
 irq_ptr:    .res 2              ; Location of IRQ routine
 
