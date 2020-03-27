9a4203ecacb68e973984e320835a8270c8815810
Kris Foster <kris.foster@gmail.com>
2020-03-26 21:20:43 -0700

    Rebrand a little to k64

--- examples/c/main.c
+++ examples/c/main.c
@@ -1,7 +1,7 @@
 // A simplified version of the example at
 // https://cc65.github.io/doc/customizing.html
 
-extern void __fastcall__ puts(char *str);
+#include <stdio.h>
 
 int main () {
 	puts("Hello, World!");        //  Transmit "Hello World!"
